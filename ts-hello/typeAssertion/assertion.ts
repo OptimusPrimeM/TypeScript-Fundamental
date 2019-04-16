5014c461b7b96def77e826bd8d952a9d781271f0
Menik Tennakoon <39112594+OptimusPrimeM@users.noreply.github.com>
2019-04-16 09:59:49 +0600

    Update assertion.ts

--- ts-hello/typeAssertion/assertion.ts
+++ ts-hello/typeAssertion/assertion.ts
@@ -1,9 +1,16 @@
 
-let v:String;
-let p :boolean;
+let v;
+let p: boolean;
+let q: boolean;
 
 v = "abc";
-p = (v as String).endsWith('c'); 
 
-console.log("sasA");
+/* Type assertions */
+
+/*1 st way*/
+p = (<string> v).endsWith('c');
+
+q = (v as string).endsWith('c');
+
 console.log(p);
+console.log(q);
